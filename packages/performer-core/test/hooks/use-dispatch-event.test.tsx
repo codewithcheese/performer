37f50f52ee6a7eecca4a1865d1a0d4909db159a5
codewithcheese <mr.tmanning@gmail.com>
2024-02-06 02:05:56 +1000

    Add subclass example to `useDispatchEvent` test

--- packages/performer-core/test/hooks/use-dispatch-event.test.tsx
+++ packages/performer-core/test/hooks/use-dispatch-event.test.tsx
@@ -1,24 +1,32 @@
 import { expect, test } from "vitest";
 import { Performer, useDispatchEvent } from "../../src/index.js";
 
+class MyEvent extends CustomEvent<{ value: number }> {
+  constructor(detail: { value: number }) {
+    super("my-event", { detail });
+  }
+}
+
 declare module "../../src/index.js" {
   interface PerformerEventMap {
     test: CustomEvent<{ value: number }>;
+    "my-event": MyEvent;
   }
 }
 
 test("should dispatch event from component", async () => {
   async function App() {
     const dispatchEvent = useDispatchEvent();
-    dispatchEvent(new CustomEvent("test", { detail: { value: 1 } }));
+    dispatchEvent(new CustomEvent("test", { detail: { value: 42 } }));
+    dispatchEvent(new MyEvent({ value: 1337 }));
     return () => <assistant>How may I serve the?</assistant>;
   }
   const performer = new Performer(<App />);
-  let eventValue = undefined;
-  performer.addEventListener("test", (evt) => {
-    eventValue = evt.detail.value;
+  let eventValues: number[] = [];
+  performer.addEventListener("*", (evt) => {
+    if ("value" in evt.detail) eventValues.push(evt.detail.value);
   });
   performer.start();
   await performer.waitUntilSettled();
-  expect(eventValue).toEqual(1);
+  expect(eventValues).toEqual([42, 1337]);
 });
