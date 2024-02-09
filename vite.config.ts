82e45ef44d0bdbdfcfd868b1fae32b586b032178
codewithcheese <mr.tmanning@gmail.com>
2024-02-09 20:20:51 +1000

    Vite only include test

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from "vitest/config";
 export default defineConfig({
+  test: {
+    include: ["**/test/*.test.*"],
+  },
   resolve: {
     alias: {
       "@performer/core/jsx-dev-runtime": "./packages/performer-core/src/jsx",
