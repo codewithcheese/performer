5cda6911c7e070ecc8879476cdb8b6c7e6fe7e66
codewithcheese <mr.tmanning@gmail.com>
2024-02-09 19:42:50 +1000

    Fix app path

--- packages/performer-ui/playwright.config.ts
+++ packages/performer-ui/playwright.config.ts
@@ -3,7 +3,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const appPath = path.join(__dirname, "./test/apps");
+const appPath = path.join(__dirname, "./e2e/apps");
 
 /**
  * See https://playwright.dev/docs/test-configuration.
