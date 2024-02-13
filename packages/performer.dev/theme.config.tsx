bf16aef9cdf8264d4d2994717b77962dcef42c22
codewithcheese <mr.tmanning@gmail.com>
2024-02-13 23:07:31 +1000

    Add doc site

---
+++ packages/performer.dev/theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Performer</span>,
+  project: {
+    link: 'https://github.com/codewithcheese/performer',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/codewithcheese/performer/tree/master/packages/performer.dev',
+  footer: {
+    text: '',
+  },
+}
+
+export default config
