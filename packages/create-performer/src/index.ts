aaf29ce5c67b3c38af474e58f1995733f7531214
codewithcheese <mr.tmanning@gmail.com>
2024-02-16 21:36:29 +1000

    Fix rename gitignore in template
    npm excludes .gitignore and every create app developer ever has made this mistake :-)
    https://github.com/dworak-dev/cli/issues/64

--- packages/create-performer/src/index.ts
+++ packages/create-performer/src/index.ts
@@ -1,7 +1,7 @@
 import * as fs from "fs";
 import * as path from "path";
 import parser from "gitignore-parser";
-import { mkdirp } from "./utils.js";
+import { copy, mkdirp } from "./utils.js";
 import { fileURLToPath } from "url";
 import glob from "tiny-glob/sync.js";
 
@@ -27,11 +27,11 @@ function write_template_files(
 ) {
   const relPath = path.basename(__dirname) === "src" ? "../" : "../../";
   const templateDir = path.join(__dirname, relPath, `templates/${template}`);
-  const gitignore_file = path.join(templateDir, ".gitignore");
-  if (!fs.existsSync(gitignore_file)) {
-    throw new Error(`"${template}" template must have a .gitignore file`);
+  const gitignoreFile = path.join(templateDir, "dot_gitignore");
+  if (!fs.existsSync(gitignoreFile)) {
+    throw new Error(`"${template}" template must have a "dot_gitignore" file`);
   }
-  const gitignore = parser.compile(fs.readFileSync(gitignore_file, "utf-8"));
+  const gitignore = parser.compile(fs.readFileSync(gitignoreFile, "utf-8"));
 
   const packageJson = fs
     .readFileSync(path.join(templateDir, "package.template.json"))
@@ -41,12 +41,15 @@ function write_template_files(
     packageJson.replace(/~TODO~/g, name),
   );
 
+  copy(gitignoreFile, path.join(outputDir, ".gitignore"));
+
   const files = glob("**/*", { cwd: templateDir, filesOnly: true, dot: true });
   files.forEach((file) => {
     if (
       !gitignore.accepts(file) ||
       file === "package.json" ||
-      file === "package.template.json"
+      file === "package.template.json" ||
+      file === "dot_gitignore"
     ) {
       return;
     }
