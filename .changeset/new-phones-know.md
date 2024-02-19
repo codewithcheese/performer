7fb8f2bd7373c51d22a3413870acf30e942d9500
Tom Manning <mr.tmanning@gmail.com>
2024-02-19 22:20:33 +1000

    Fix playground vite dependency bundling when using playground cli (#25)

---
+++ .changeset/new-phones-know.md
@@ -0,0 +1,6 @@
+---
+"@performer/playground": patch
+"create-performer": patch
+---
+
+Fix playground vite dependency bundling when using playground cli
--- packages/performer-playground/vite.config.ts
+++ packages/performer-playground/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
-import { transform } from "esbuild";
+import path from "path";
+
+const examplesPath = path.join(__dirname, "./e2e/apps");
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,32 +10,29 @@ export default defineConfig({
     include: ["test/**.*"],
   },
   plugins: [
-    {
-      name: "performer-jsx-transformer",
-      async transform(code, id) {
-        if (
-          typeof process.env.VITE_PERFORMER_APP_PATH === "string" &&
-          id.startsWith(process.env.VITE_PERFORMER_APP_PATH)
-        ) {
-          console.log(`Performer JSX transformed ${id}`);
-          const result = await transform(code, {
-            loader: "jsx",
-            jsx: "automatic",
-            jsxImportSource: "@performer/core",
-            sourcefile: id,
-          });
-          return {
-            code: result.code,
-            map: result.map,
-          };
-        }
-      },
-    },
+    // {
+    //   name: "performer-jsx-transformer",
+    //   async transform(code, id) {
+    //     if (id.startsWith(examplesPath)) {
+    //       console.log(`Performer JSX transformed ${id}`);
+    //       const result = await transform(code, {
+    //         loader: "jsx",
+    //         jsx: "automatic",
+    //         jsxImportSource: "@performer/core",
+    //         sourcefile: id,
+    //       });
+    //       return {
+    //         code: result.code,
+    //         map: result.map,
+    //       };
+    //     }
+    //   },
+    // },
     react(),
   ],
   resolve: {
     alias: {
-      "@app": process.env.VITE_PERFORMER_APP_PATH,
+      "@app": examplesPath,
     },
   },
 });
