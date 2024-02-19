70661f0cd9794f64037c97efd37fd39781264a99
codewithcheese <mr.tmanning@gmail.com>
2024-02-19 22:18:42 +1000

    Restore e2e/www as default for vitest dev

--- packages/performer-playground/vite.config.ts
+++ packages/performer-playground/vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
-import { transform } from "esbuild";
 import path from "path";
 
-const examplesPath = path.join(__dirname, "../../examples");
+const examplesPath = path.join(__dirname, "./e2e/apps");
 
 // https://vitejs.dev/config/
 export default defineConfig({
