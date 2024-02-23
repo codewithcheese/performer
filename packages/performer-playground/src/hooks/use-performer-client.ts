5179ddf53dd550ae4c6a54f6eacbd0125467bd9a
Tom Manning <mr.tmanning@gmail.com>
2024-02-24 02:53:05 +1000

    Add sidebar and router to playground (#40)

--- packages/performer-playground/src/hooks/use-performer-client.ts
+++ packages/performer-playground/src/hooks/use-performer-client.ts
@@ -24,6 +24,7 @@ export function usePerformerClient(app: Component<any> | null) {
       return;
     }
     try {
+      setEvents([]);
       const performer = new Performer(jsx(app, {}));
       setPerformer(performer);
 
