3c49a22512c630d7db12dc833b19e7f2cece2f10
codewithcheese <mr.tmanning@gmail.com>
2024-02-27 21:54:51 +1000

    Debugging sandpack without first char logic

--- packages/performer-playground/src/components/MessageMarkdown.tsx
+++ packages/performer-playground/src/components/MessageMarkdown.tsx
@@ -21,32 +21,35 @@ export const MessageMarkdown: FC<MessageMarkdownProps> = ({ content }) => {
           return <img className="max-w-[67%]" {...props} />;
         },
         code({ node, className, children, ...props }) {
+          // const childArray = React.Children.toArray(children);
+          // const firstChild = childArray[0] as React.ReactElement;
+          // const firstChildAsString = React.isValidElement(firstChild)
+          //   ? (firstChild as React.ReactElement).props.children
+          //   : firstChild;
+          //
+          // if (firstChildAsString === "▍") {
+          //   return <span className="mt-1 animate-pulse cursor-default">▍</span>;
+          // }
+          //
+          // if (typeof firstChildAsString === "string") {
+          //   childArray[0] = firstChildAsString.replace("`▍`", "▍");
+          // }
+          //
+          // const match = /language-(\w+)/.exec(className || "");
+          //
+          // if (
+          //   typeof firstChildAsString === "string" &&
+          //   !firstChildAsString.includes("\n")
+          // ) {
+          //   return (
+          //     <code className={className} {...props}>
+          //       {childArray}
+          //     </code>
+          //   );
+          // }
+
           const childArray = React.Children.toArray(children);
-          const firstChild = childArray[0] as React.ReactElement;
-          const firstChildAsString = React.isValidElement(firstChild)
-            ? (firstChild as React.ReactElement).props.children
-            : firstChild;
-
-          if (firstChildAsString === "▍") {
-            return <span className="mt-1 animate-pulse cursor-default">▍</span>;
-          }
-
-          if (typeof firstChildAsString === "string") {
-            childArray[0] = firstChildAsString.replace("`▍`", "▍");
-          }
-
           const match = /language-(\w+)/.exec(className || "");
-
-          if (
-            typeof firstChildAsString === "string" &&
-            !firstChildAsString.includes("\n")
-          ) {
-            return (
-              <code className={className} {...props}>
-                {childArray}
-              </code>
-            );
-          }
 
           return (
             <MessageCodeBlock
