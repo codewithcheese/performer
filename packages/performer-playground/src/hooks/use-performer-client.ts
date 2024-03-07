3db7fd9e400a2712f3f68337f9d9a97cba95f422
codewithcheese <mr.tmanning@gmail.com>
2024-03-07 19:33:45 +1000

    Remove console log

--- packages/performer-playground/src/hooks/use-performer-client.ts
+++ packages/performer-playground/src/hooks/use-performer-client.ts
@@ -29,7 +29,6 @@ export function usePerformerClient(app: Component<any> | null) {
       setPerformer(performer);
 
       performer.addEventListener("*", (event) => {
-        console.log("Received event", event);
         if (!(event instanceof PerformerDeltaEvent)) {
           return setEvents((prevEvents) => [...prevEvents, event]);
         }
@@ -39,19 +38,15 @@ export function usePerformerClient(app: Component<any> | null) {
               prev instanceof PerformerDeltaEvent &&
               prev.detail.uid === event.detail.uid,
           );
-          console.log("Previous event", previous);
           if (!previous) {
-            console.log("Appending delta event");
             return [...prevEvents, event];
           }
           concatDelta(previous.detail.delta, event.detail.delta);
-          console.log("concat delta", previous, event);
           const spliced = prevEvents.toSpliced(
             prevEvents.length - 1,
             1,
             previous,
           );
-          console.log("spliced", spliced);
           return spliced;
         });
       });
