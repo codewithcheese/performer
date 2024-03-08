3fdfe0e872fd7e9e915a5f8e57fb08e08d5a5001
codewithcheese <mr.tmanning@gmail.com>
2024-03-08 18:32:45 +1000

    Fix playwright webserver command

--- packages/create-performer/templates/default/playwright.config.ts
+++ packages/create-performer/templates/default/playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   ],
 
   webServer: {
-    command: `npm run dev`,
+    command: `npm run playground`,
     url: "http://localhost:3011",
     reuseExistingServer: !process.env.CI,
     timeout: 10_000,
