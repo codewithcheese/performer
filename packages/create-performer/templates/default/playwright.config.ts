aec190250c4d40d8225c161f0e3ac4ec8b269642
Tom Manning <mr.tmanning@gmail.com>
2024-03-08 18:08:23 +1000

    Add e2e test to create-performer default template (#81)

---
+++ packages/create-performer/templates/default/playwright.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig, devices } from "@playwright/test";
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: "./test",
+  testMatch: "**/*.e2e.ts", // This line ensures only .e2e.ts files are matched
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: "html",
+  use: {
+    baseURL: "http://localhost:3011",
+    trace: "on-first-retry",
+  },
+
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+  ],
+
+  webServer: {
+    command: `npm run dev`,
+    url: "http://localhost:3011",
+    reuseExistingServer: !process.env.CI,
+    timeout: 10_000,
+  },
+});
